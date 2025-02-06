bfc27b5fca4add6cc22869e7cab5c65da8d67454
Tomislav <tomislav.jakopec@gmail.com>
2025-02-06 17:22:21 -0500

    Add color picker, add random RGB on each iteration.

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ const padWidth = sketchPad.offsetWidth;
 const padHeight = sketchPad.offsetHeight;
 const inputField = document.getElementById("grid-size-input");
 const warningMessage = document.getElementById("warning");
+const sketchMode = ["black", "pickColor", "random"];
+const colorPicker = document.getElementById("selectedColor");
+const colorPickerRadio = document.getElementById("radioPick");
+let selectedColor = colorPicker.value;
+const colorModeRadios = document.querySelectorAll('input[name="mode"]');
+let selectedColorModeRadio = document.querySelector(
+  'input[name="mode"]:checked'
+).value;
+
+let colorToUse = "black";
 
 let gridSize = document
   .getElementById("grid-size-input")
@@ -10,9 +20,39 @@ let gridSize = document
 let pixelBox = document.createElement("div");
 pixelBox.className = "pixel";
 
-inputField.addEventListener("keyup", function () {
+// Listeners
+
+inputField.addEventListener("change", function () {
   validateInputSize(inputField.value);
 });
+
+const adjustButton = document.getElementById("size-button");
+
+adjustButton.addEventListener("click", function () {
+  const userInput = document.getElementById("grid-size-input").value;
+
+  gridSize = parseInt(userInput);
+  populateAndListen();
+});
+
+colorPicker.addEventListener("click", function () {
+  colorPickerRadio.checked = "true";
+});
+
+colorPicker.addEventListener("change", function () {
+  colorToUse = colorPicker.value;
+});
+
+colorModeRadios.forEach((radio) =>
+  radio.addEventListener("click", function () {
+    selectedColorModeRadio = document.querySelector(
+      'input[name="mode"]:checked'
+    ).value;
+    setColor(selectedColorModeRadio);
+  })
+);
+
+// Listeners end
 
 function populatePad() {
   let pixelDimensions = Math.floor((10 * padWidth) / gridSize) / 10;
@@ -29,19 +69,30 @@ function listenForMouse() {
   let pixelBoxes = document.getElementsByClassName("pixel");
   for (let i = 0; i < gridSize * gridSize; i++) {
     pixelBoxes[i].addEventListener("mouseover", function () {
-      pixelBoxes[i].style.background = "black";
+      pixelBoxes[i].style.background = colorToUse;
+      if (selectedColorModeRadio == "random") {
+        randomColor();
+      }
     });
   }
 }
 
-const adjustButton = document.getElementById("size-button");
+function setColor(key) {
+  if (key == "black") {
+    colorToUse = "black";
+  } else if (key == "random") {
+    colorToUse = randomColor();
+  } else if (key == "pickColor") {
+    colorToUse = colorPicker.value;
+  }
+}
 
-adjustButton.addEventListener("click", function () {
-  const userInput = document.getElementById("grid-size-input").value;
-
-  gridSize = parseInt(userInput);
-  populateAndListen();
-});
+function randomColor() {
+  const color = [...Array(6)]
+    .map(() => Math.floor(Math.random() * 16).toString(16))
+    .join("");
+  colorToUse = "#" + color;
+}
 
 function populateAndListen() {
   clearPad();
