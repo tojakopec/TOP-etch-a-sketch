61a1f5991ccbace3f2e2e6e215a06c5076f5a7c8
Tomislav <tomislav.jakopec@gmail.com>
2025-02-06 10:48:14 -0500

    Add validation of input.

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const sketchPad = document.getElementById("sketch-zone");
 const padWidth = sketchPad.offsetWidth;
 const padHeight = sketchPad.offsetHeight;
+const inputField = document.getElementById("grid-size-input");
+const warningMessage = document.getElementById("warning");
+
 let gridSize = document
   .getElementById("grid-size-input")
   .placeholder.split(": ")[1];
 let pixelBox = document.createElement("div");
 pixelBox.className = "pixel";
+
+inputField.addEventListener("keyup", function () {
+  validateInputSize(inputField.value);
+});
 
 function populatePad() {
   let pixelDimensions = Math.floor((10 * padWidth) / gridSize) / 10;
@@ -31,6 +38,7 @@ const adjustButton = document.getElementById("size-button");
 
 adjustButton.addEventListener("click", function () {
   const userInput = document.getElementById("grid-size-input").value;
+
   gridSize = parseInt(userInput);
   populateAndListen();
 });
@@ -43,6 +51,17 @@ function populateAndListen() {
 
 function clearPad() {
   sketchPad.innerHTML = "";
+}
+
+function validateInputSize(inputFieldValue) {
+  if (inputFieldValue == "") {
+    return;
+  } else if (inputFieldValue > 200 || inputFieldValue < 1) {
+    inputField.value = 200;
+    warningMessage.style.visibility = "visible";
+  } else {
+    warningMessage.style.visibility = "hidden";
+  }
 }
 
 populateAndListen();
